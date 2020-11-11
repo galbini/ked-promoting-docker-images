f2799a984fcada9240877565db95650e88d58186
Guillaume Albini <6636889+galbini@users.noreply.github.com>
2020-11-11 18:57:09 +0100

    exist

--- scripts/image-exists.sh
+++ scripts/image-exists.sh
@@ -5,4 +5,9 @@ FILTER=$2
 
 NB_IMAGE=$(gcloud container images list-tags $IMAGE --filter=$FILTER --limit=1|wc -l)
 
-echo ${NB_IMAGE}
+if [[ $NB_IMAGE -eq 0 ]]
+then
+    echo true
+else
+    echo false
+fi
